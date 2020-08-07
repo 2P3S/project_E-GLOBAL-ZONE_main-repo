6f4a2d0d9df529ca3c9c75c4d9055fdecc4f1f84
LeeJaeBae <nea4182@gmail.com>
2020-08-08 05:29:28 +0900

    Mobile :: ApplicationForm mockup

--- src/components/mobile/Calendar.js
+++ src/components/mobile/Calendar.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Calendar() {
 	return (
 		<div className="calandar">
-			<a href="/global/mobile/student_apply.php">
+			<a href="/reservation/1">
 				<img src="/global/mobile/img/calendar_ex.png" alt="캘린더 예시" />
 			</a>
 		</div>
