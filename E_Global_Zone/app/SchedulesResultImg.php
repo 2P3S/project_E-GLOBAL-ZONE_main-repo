cb3eddc07f1df9f9d3e903099be1e9e651dfae63
JeongJaeSoon <wjdowtns1006@gmail.com>
2020-08-09 17:29:23 +0900

    set migrations schedules result images

--- E_Global_Zone/app/SchedulesResultImg.php
+++ E_Global_Zone/app/SchedulesResultImg.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class SchedulesResultImg extends Model
 {
-    //
+    protected $primaryKey = 'sch_id';
+
+    protected $fillable = [
+        'start_img_url',
+        'end_img_url'
+    ];
 }
