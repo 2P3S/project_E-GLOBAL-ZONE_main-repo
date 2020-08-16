355b3c0a129c02c7fee4471432e951f3e402ceea
JeongJaeSoon <wjdwotns1006@gmail.com>
2020-08-16 15:14:41 +0900

    Test scheduler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Department;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -16,16 +17,25 @@ class Kernel extends ConsoleKernel
         //
     ];
 
+    /*
+     * 작성일 : 2020.08.16
+     * 작성자 : 정재순
+     * 작성내용
+     *  - 스케줄러 등록
+     */
     /**
      * Define the application's command schedule.
      *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @param \Illuminate\Console\Scheduling\Schedule $schedule
      * @return void
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule->call(function () {
+            Department::create([
+                'dept_name' => now()
+            ]);
+        })->everyMinute();
     }
 
     /**
@@ -35,7 +45,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
