e2f117596171834495e398823388ffff27329b85
LeeJaeBae <nea4182@gmail.com>
2020-08-27 23:59:45 +0900

    Manager :: Foreigner Create Schedules

--- E_Global_Zone-Web_build/src/routes/Web/Manager/index.js
+++ E_Global_Zone-Web_build/src/routes/Web/Manager/index.js
@@ -1,5 +1,6 @@
 import Schedules from "./Schedules/Schedules";
 import Settings from "./Settings/Settings";
 import Students from "./Students/Students";
+import Section from "./Section/Section";
 
-export { Schedules, Settings, Students };
+export { Schedules, Settings, Students, Section };
