158c81ab5adb3492c30be3eb24aec528c16ed9f3
JeongJaeSoon <wjdwotns1006@gmail.com>
2020-08-29 02:01:42 +0900

    메일 전송 기능 구현

---
+++ E_Global_Zone/app/Mail/OrderShipped.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class OrderShipped extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->subject('Hello World');
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('view.name');
+    }
+}
