f907ad7aa91ce0577823348d2098fe1d8f0b5754
LeeJaeBae <nea4182@gmail.com>
2020-08-30 23:14:47 +0900

    small bug fix

--- E_Global_Zone-Web_build/src/app/Routers/Router.Login.js
+++ E_Global_Zone-Web_build/src/app/Routers/Router.Login.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { Switch, Redirect, Route } from "react-router-dom";
-import Login, { KoreanLogin, MobileLogin } from "../../routes/Login/Login";
+import Login, { KoreanLogin, MobileLogin, AdminLogin } from "../../routes/Login/Login";
 import SignUp from "../../routes/Login/SignUp";
 import { isMobile } from "react-device-detect";
+import ResetPassword from "../../routes/Login/ResetPassword";
 /**
  * LoginRouter - Router for Login
  * @returns {JSX.Element}
@@ -15,11 +16,13 @@ export const LoginRouter = () => {
 
 			<Route exact path="/foreigner" component={Login} />
 			<Route exact path="/student" component={KoreanLogin} />
-			<Route exact path="/manager" component={""} />
+			<Route exact path="/admin" component={AdminLogin} />
 
-			<Route path="/" component={isMobile ? MobileLogin : KoreanLogin} />
+			<Route exact path="/:userClass/password" component={ResetPassword} />
 
 			<Route exact path="/korean/sign-up" component={SignUp} />
+
+			<Route path="/" component={isMobile ? MobileLogin : KoreanLogin} />
 		</Switch>
 	);
 };
