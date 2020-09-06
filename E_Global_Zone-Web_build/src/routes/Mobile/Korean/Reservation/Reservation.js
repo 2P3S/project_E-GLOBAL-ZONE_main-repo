1cb16e07684d04cbbbe7a4cbc3d09d90c4e90f4c
LeeJaeBae <nea4182@gmail.com>
2020-09-06 20:58:12 +0900

    Korean :: Reservation renewal

--- E_Global_Zone-Web_build/src/routes/Mobile/Korean/Reservation/Reservation.js
+++ E_Global_Zone-Web_build/src/routes/Mobile/Korean/Reservation/Reservation.js
@@ -29,12 +29,100 @@ export default function Reservation() {
 
 	return (
 		<>
-			<Calendar />
-			{typeof data === "object" && data.hasOwnProperty("data") ? (
-				<List data={data} />
-			) : (
-				<p className="reserv_info">신청 된 예약이 없습니다.</p>
-			)}
+			<div className="mtab">
+				<ul className="no3">
+					<li>
+						<a href="student_reservation_type2.php" className="on">
+							예약 조회
+						</a>
+					</li>
+					<li>
+						<a href="student_schedule.php">스케줄 조회</a>
+					</li>
+					<li>
+						<a href="student_result.php">결과 관리</a>
+					</li>
+				</ul>
+			</div>
+
+			<div className="wrap bg">
+				<div className="reserv_status">
+					<p className="tit">실시간 예약 현황</p>
+					<ul>
+						<li className="yellow">
+							<span>3</span>예약 대기
+						</li>
+						<li>
+							<span>7</span>예약 완료
+						</li>
+						<li>
+							<span>4</span>결과 대기
+						</li>
+					</ul>
+				</div>
+				<div className="reserv_list">
+					<p className="tit">예약 관리</p>
+					<ui className="mainMenu">
+						<li className="item status01" id="wait_reservation">
+							<a href="#wait_reservation" className="btn">
+								<span>예약 대기</span>
+							</a>
+							<div className="subMenu">
+								<div>
+									<p className="left">
+										[영어]T구글구글리<span>12:30:00 ~ 12:50:00</span>
+									</p>
+									<p className="right">예약대기</p>
+								</div>
+								<div>
+									<p className="left">
+										[영어]T구글구글리<span>12:30:00 ~ 12:50:00</span>
+									</p>
+									<p className="right">예약대기</p>
+								</div>
+							</div>
+						</li>
+						<li className="item status02" id="reservation_complete">
+							<a href="#reservation_complete" className="btn">
+								<span>예약 완료</span>
+							</a>
+							<div className="subMenu">
+								<div>
+									<p className="left">
+										[영어]T구글구글리<span>12:30:00 ~ 12:50:00</span>
+									</p>
+									<p className="right zoom_info">접속정보</p>
+								</div>
+								<div>
+									<p className="left">
+										[영어]T구글구글리<span>12:30:00 ~ 12:50:00</span>
+									</p>
+									<p className="right zoom_info">접속정보</p>
+								</div>
+							</div>
+						</li>
+						<li className="item status03" id="progress_complete">
+							<a href="#progress_complete" className="btn">
+								<span>결과 대기</span>
+							</a>
+							<div className="subMenu">
+								<div>
+									<p className="left">
+										[영어]T구글구글리<span>12:30:00 ~ 12:50:00</span>
+									</p>
+									<p className="right">예약대기</p>
+								</div>
+								<div>
+									<p className="left">
+										[영어]T구글구글리<span>12:30:00 ~ 12:50:00</span>
+									</p>
+									<p className="right">예약대기</p>
+								</div>
+							</div>
+						</li>
+					</ui>
+				</div>
+			</div>
 		</>
 	);
 }
