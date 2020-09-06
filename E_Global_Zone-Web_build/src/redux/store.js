99d9e895b9d847ab6e38fb1db751e936993e0298
LeeJaeBae <nea4182@gmail.com>
2020-09-06 17:53:02 +0900

    redux devTool setting

--- E_Global_Zone-Web_build/src/redux/store.js
+++ E_Global_Zone-Web_build/src/redux/store.js
@@ -1,11 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import user from "./userSlice/userSlice";
-import conf from './confSlice/confSlice';
-import manager from './managerSlice/managerSlice'
+import conf from "./confSlice/confSlice";
+import manager from "./managerSlice/managerSlice";
 
 /**
  * ReduxStore
  */
 export default configureStore({
 	reducer: { user, conf, manager },
+	devTools: !process.env.REACT_APP_PRODUCT_MODE,
 });
