278cd109d6c690fd5a2d3b131a2aeeffabcc36c1
LeeJaeBae <nea4182@gmail.com>
2020-09-17 22:51:22 +0900

    rebuild

--- E_Global_Zone-Web_build/build/service-worker.js
+++ E_Global_Zone-Web_build/build/service-worker.js
@@ -14,9 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-
   "/precache-manifest.9a251350ff881db8355868ad3f664a31.js"
-
 );
 
 self.addEventListener('message', (event) => {
