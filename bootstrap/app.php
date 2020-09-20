be2fa5a6b48fd7e4c4913eb72cfcbc90bd65b168
JeongJaeSoon <wjdwotns1006@gmail.com>
2020-09-21 01:53:22 +0900

    add Access-Control-Expose-Headers

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: *');
 header('Access-Control-Allow-Headers: *');
+header('Access-Control-Expose-Headers: content-disposition');
 
 /*
 |--------------------------------------------------------------------------
