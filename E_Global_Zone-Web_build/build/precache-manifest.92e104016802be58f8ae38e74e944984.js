32da13c83f7acea070fe0890dc69c06837b355b8
LeeJaeBae <nea4182@gmail.com>
2020-09-24 01:32:43 +0900

    test build

--- E_Global_Zone-Web_build/build/precache-manifest.92e104016802be58f8ae38e74e944984.js
+++ E_Global_Zone-Web_build/build/precache-manifest.92e104016802be58f8ae38e74e944984.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "48a2be5ef2235ce5aeefc6e738412a8f",
+    "revision": "105f90b2604e993172e74c9e00bd3465",
     "url": "/index.html"
   },
   {
-    "revision": "f51815c78fee6c47acca",
-    "url": "/static/js/2.a67401cb.chunk.js"
+    "revision": "0f3dee34258c3a2167cf",
+    "url": "/static/js/2.07ac497a.chunk.js"
   },
   {
     "revision": "60ab5c56d6bce4f14256374f373658d7",
-    "url": "/static/js/2.a67401cb.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.07ac497a.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "ef15051b66e2c2283ca8",
-    "url": "/static/js/main.f80e0f05.chunk.js"
+    "revision": "dd540c3291c61f31fcfd",
+    "url": "/static/js/main.74bf57d1.chunk.js"
   },
   {
     "revision": "3884bc877ed86415334c",
--- E_Global_Zone-Web_build/build/service-worker.js
+++ E_Global_Zone-Web_build/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3bc6ab45287459e7b67e42ad8019bbb7.js"
+  "/precache-manifest.92e104016802be58f8ae38e74e944984.js"
 );
 
 self.addEventListener('message', (event) => {
