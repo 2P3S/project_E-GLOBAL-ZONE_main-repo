349df21892f31b49b44394d6ab9359bfa9d4fb5f
SeungHyun Cho <c0305sh@naver.com>
2020-09-26 18:00:25 +0900

    [0.8.2] Hot-fix :: 버전 체크

--- E_Global_Zone/app/Http/Controllers/SettingController.php
+++ E_Global_Zone/app/Http/Controllers/SettingController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Setting;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Config;
 
 class SettingController extends Controller
 {
