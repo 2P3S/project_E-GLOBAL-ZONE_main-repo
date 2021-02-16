4596552b858466774ee4a4edebf3dcbd038c6a50
LeeJaeBae <nea4182@gmail.com>
2021-02-16 20:35:37 +0900

    Admin - Students status reset btn is done

--- E_Global_Zone-Web_build/src/routes/Web/Manager/Settings/Settings.js
+++ E_Global_Zone-Web_build/src/routes/Web/Manager/Settings/Settings.js
@@ -1,16 +1,21 @@
-import React, { useEffect, useState } from "react";
-import { getAdminSetting, postAdminSetting } from "../../../../api/admin/setting";
-import useModal from "../../../../modules/hooks/useModal";
-import Modal from "../../../../components/common/modal/Modal";
-import moment from "moment";
-import CreateSection from "../../../../components/common/modal/CreateSection";
-import GetSections from "../../../../components/common/modal/GetSections";
+import React, { useEffect, useState } from 'react';
+import {
+	getAdminSetting,
+	getAdminSettingReset,
+	postAdminSetting,
+} from '../../../../api/admin/setting';
+import useModal from '../../../../modules/hooks/useModal';
+import Modal from '../../../../components/common/modal/Modal';
+import moment from 'moment';
+import CreateSection from '../../../../components/common/modal/CreateSection';
+import GetSections from '../../../../components/common/modal/GetSections';
 
-import InsertForeignerStudent from "../../../../components/common/modal/InsertForeignerStudent";
-import Loader from "../../../../components/common/Loader";
-import CreateDept from "../../../../components/common/modal/CreateDept";
-import { getAdminExportForeigner } from "../../../../api/admin/export";
-import WirteNotice from "../../../../components/common/modal/WirteNotice";
+import InsertForeignerStudent from '../../../../components/common/modal/InsertForeignerStudent';
+import Loader from '../../../../components/common/Loader';
+import CreateDept from '../../../../components/common/modal/CreateDept';
+import { getAdminExportForeigner } from '../../../../api/admin/export';
+import WirteNotice from '../../../../components/common/modal/WirteNotice';
+import DeleteModal from '../../../../components/common/modal/DeleteModal';
 
 /**
  * Manager :: 시스템 환경설정
@@ -42,13 +47,22 @@ export default function Settings() {
 		handleOpen: handleOpenForCreateDept,
 		handleClose: handleCloseForCreateDept,
 	} = useModal();
+	const {
+		isOpen: isOpenForReset,
+		handleOpen: handleOpenForReset,
+		handleClose: handleCloseForReset,
+	} = useModal();
 
 	const handleChange = (key, value) => {
 		setPostSettings({ ...postSettings, [key]: parseInt(value) });
 	};
+	const handleReset = () => {
+		getAdminSettingReset();
+		handleCloseForReset();
+	};
 	const handleChangeMeet = () => {
-		let meet = document.getElementById("time_input_meet");
-		let rest = document.getElementById("time_input_rest");
+		let meet = document.getElementById('time_input_meet');
+		let rest = document.getElementById('time_input_rest');
 		if (meet.value < 30 && meet.value >= 0) {
 			rest.value = 30 - meet.value;
 		} else {
@@ -61,8 +75,8 @@ export default function Settings() {
 		});
 	};
 	const handleChangeRest = () => {
-		let meet = document.getElementById("time_input_meet");
-		let rest = document.getElementById("time_input_rest");
+		let meet = document.getElementById('time_input_meet');
+		let rest = document.getElementById('time_input_rest');
 		if (rest.value < 30 && rest.value >= 0) {
 			meet.value = 30 - rest.value;
 		} else {
@@ -101,37 +115,31 @@ export default function Settings() {
 	let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 	let arrayDate = [1, 2, 3, 4, 5, 6, 7];
 	return pending ? (
-		<div className="content">
-			<div className="sub_title">
-				<p className="tit">시스템 환경설정</p>
+		<div className='content'>
+			<div className='sub_title'>
+				<p className='tit'>시스템 환경설정</p>
 			</div>
 
-			<div className="set_wrap">
-				<div className="setting">
-					<div className="set_box">
-						<p className="tit ico01">예약관리 설정</p>
-						<div className="input">
-							<div className="select_tit">
-								한국인 학생 <span className="bold">하루 예약</span>
+			<div className='set_wrap'>
+				<div className='setting'>
+					<div className='set_box'>
+						<p className='tit ico01'>예약관리 설정</p>
+						<div className='input'>
+							<div className='select_tit'>
+								한국인 학생 <span className='bold'>하루 예약</span>
 							</div>
-							<div className="select_input">
+							<div className='select_input'>
 								<select
-									name="catgo1"
-									className="dropdown"
+									name='catgo1'
+									className='dropdown'
 									onChange={(e) => {
 										handleChange(`max_res_per_day`, e.target.value);
-									}}
-								>
+									}}>
 									{array.map((v) => {
 										return (
 											<option
 												id={`day-${v}`}
-												selected={
-													settings && settings.data.max_res_per_day === v
-														? true
-														: false
-												}
-											>
+												selected={settings && settings.data.max_res_per_day === v ? true : false}>
 												{v}
 											</option>
 										);
@@ -140,28 +148,22 @@ export default function Settings() {
 								<span>번</span>
 							</div>
 						</div>
-						<div className="input">
-							<div className="select_tit">
-								한 타임 <span className="bold">최대 예약</span>
+						<div className='input'>
+							<div className='select_tit'>
+								한 타임 <span className='bold'>최대 예약</span>
 							</div>
-							<div className="select_input">
+							<div className='select_input'>
 								<select
-									name="catgo1"
-									className="dropdown"
+									name='catgo1'
+									className='dropdown'
 									onChange={(e) => {
 										handleChange(`max_std_once`, e.target.value);
-									}}
-								>
+									}}>
 									{array.map((v) => {
 										return (
 											<option
 												id={`day-${v}`}
-												selected={
-													settings && settings.data.max_std_once === v
-														? true
-														: false
-												}
-											>
+												selected={settings && settings.data.max_std_once === v ? true : false}>
 												{v}
 											</option>
 										);
@@ -170,41 +172,29 @@ export default function Settings() {
 								<span>명</span>
 							</div>
 						</div>
-						<div className="input">
-							<div className="select_tit line2">
-								예약 <span className="bold">승인 및 취소</span>는{" "}
-								<span className="ex">
-									현재 기준{" "}
-									{moment(Date.now())
-										.subtract(postSettings.res_end_period, "d")
-										.format("MM.DD")}{" "}
+						<div className='input'>
+							<div className='select_tit line2'>
+								예약 <span className='bold'>승인 및 취소</span>는{' '}
+								<span className='ex'>
+									현재 기준{' '}
+									{moment(Date.now()).subtract(postSettings.res_end_period, 'd').format('MM.DD')}{' '}
 									24:00까지 완료
 								</span>
 							</div>
-							<div className="select_input">
+							<div className='select_input'>
 								<select
-									id="select_res_end_period"
-									name="catgo1"
-									className="dropdown"
+									id='select_res_end_period'
+									name='catgo1'
+									className='dropdown'
 									onChange={(e) => {
 										handleChange(`res_end_period`, e.target.value);
-									}}
-								>
+									}}>
 									{arrayDate.map((v) => {
 										return (
 											<option
 												id={`day-${v}`}
-												selected={
-													settings && settings.data.res_end_period === v
-														? true
-														: false
-												}
-												disabled={
-													settings && settings.data.res_start_period < v
-														? true
-														: false
-												}
-											>
+												selected={settings && settings.data.res_end_period === v ? true : false}
+												disabled={settings && settings.data.res_start_period < v ? true : false}>
 												{v}
 											</option>
 										);
@@ -213,41 +203,29 @@ export default function Settings() {
 								<span>일 전까지</span>
 							</div>
 						</div>
-						<div className="input">
-							<div className="select_tit line2">
-								예약 <span className="bold">신청 가능</span>한 기준일
-								<span className="ex">
-									현재 기준{" "}
-									{moment(Date.now())
-										.subtract(postSettings.res_start_period, "d")
-										.format("MM.DD")}{" "}
+						<div className='input'>
+							<div className='select_tit line2'>
+								예약 <span className='bold'>신청 가능</span>한 기준일
+								<span className='ex'>
+									현재 기준{' '}
+									{moment(Date.now()).subtract(postSettings.res_start_period, 'd').format('MM.DD')}{' '}
 									24:00부터 가능
 								</span>
 							</div>
-							<div className="select_input">
+							<div className='select_input'>
 								<select
-									id="select_res_start_period"
-									name="catgo1"
-									className="dropdown"
+									id='select_res_start_period'
+									name='catgo1'
+									className='dropdown'
 									onChange={(e) => {
 										handleChange(`res_start_period`, e.target.value);
-									}}
-								>
+									}}>
 									{arrayDate.map((v) => {
 										return (
 											<option
 												id={`day-${v}`}
-												selected={
-													settings && settings.data.res_start_period === v
-														? true
-														: false
-												}
-												disabled={
-													settings && settings.data.res_end_period > v
-														? true
-														: false
-												}
-											>
+												selected={settings && settings.data.res_start_period === v ? true : false}
+												disabled={settings && settings.data.res_end_period > v ? true : false}>
 												{v}
 											</option>
 										);
@@ -258,80 +236,71 @@ export default function Settings() {
 						</div>
 					</div>
 
-					<div className="sch_box">
-						<p className="tit ico02">스케줄관리 설정</p>
-						<p className="info">
+					<div className='sch_box'>
+						<p className='tit ico02'>스케줄관리 설정</p>
+						<p className='info'>
 							스케줄 한 타임의 기준시간은
 							<br />
-							<span>
-								{postSettings.once_meet_time + postSettings.once_rest_time}분
-							</span>
+							<span>{postSettings.once_meet_time + postSettings.once_rest_time}분</span>
 							으로 자동계산됩니다.
 						</p>
-						<div className="input_area">
-							<div className="input">
-								<div className="select_tit">
-									<span className="bold">미팅 시간</span>
+						<div className='input_area'>
+							<div className='input'>
+								<div className='select_tit'>
+									<span className='bold'>미팅 시간</span>
 								</div>
-								<div className="select_input">
+								<div className='select_input'>
 									{/*<select name="catgo1" className="dropdown">*/}
 									{/*    */}
 									{/*</select>*/}
 									<input
-										type="number"
+										type='number'
 										defaultValue={settings.data.once_meet_time}
 										onChange={handleChangeMeet}
-										id="time_input_meet"
+										id='time_input_meet'
 									/>
 									<span>분</span>
 								</div>
 							</div>
-							<div className="input">
-								<div className="select_tit">
-									<span className="bold">쉬는 시간</span>
+							<div className='input'>
+								<div className='select_tit'>
+									<span className='bold'>쉬는 시간</span>
 								</div>
-								<div className="select_input">
+								<div className='select_input'>
 									<input
-										type="number"
+										type='number'
 										defaultValue={settings.data.once_rest_time}
 										onChange={handleChangeRest}
-										id="time_input_rest"
+										id='time_input_rest'
 									/>
 									<span>분</span>
 								</div>
 							</div>
 						</div>
-						<p class="warning_txt">
-							설정 이후에 생성되는 스케줄부터 적용되며, 학기 시작 중에는{" "}
-							<span>변경 불가</span>
+						<p class='warning_txt'>
+							설정 이후에 생성되는 스케줄부터 적용되며, 학기 시작 중에는 <span>변경 불가</span>
 						</p>
 					</div>
 				</div>
 
-				<div className="regulation">
-					<p className="tit ico03">규정관리</p>
-					<div className="input">
-						<div className="select_tit">
-							<span className="bold">노쇼 최대</span>
+				<div className='regulation'>
+					<p className='tit ico03'>규정관리</p>
+					<div className='input'>
+						<div className='select_tit'>
+							<span className='bold'>노쇼 최대</span>
 						</div>
-						<div className="select_input">
+						<div className='select_input'>
 							<select
-								name="catgo1"
-								className="dropdown"
+								name='catgo1'
+								className='dropdown'
 								onChange={(e) => {
 									handleChange(`max_absent`, e.target.value);
-								}}
-							>
+								}}>
 								{array.map((v) => {
 									return (
 										<option
 											id={`day-${v}`}
-											selected={
-												settings && settings.data.max_absent === v
-													? true
-													: false
-											}
-										>
+											selected={settings && settings.data.max_absent === v ? true : false}>
 											{v}
 										</option>
 									);
@@ -340,28 +309,22 @@ export default function Settings() {
 							<span>까지</span>
 						</div>
 					</div>
-					<div className="input">
-						<div className="select_tit">
-							<span className="bold">패널티 부여</span>
+					<div className='input'>
+						<div className='select_tit'>
+							<span className='bold'>패널티 부여</span>
 						</div>
-						<div className="select_input">
+						<div className='select_input'>
 							<select
-								name="catgo1"
-								className="dropdown"
+								name='catgo1'
+								className='dropdown'
 								onChange={(e) => {
 									handleChange(`min_absent`, e.target.value);
-								}}
-							>
+								}}>
 								{array.map((v) => {
 									return (
 										<option
 											id={`day-${v}`}
-											selected={
-												settings && settings.data.min_absent === v
-													? true
-													: false
-											}
-										>
+											selected={settings && settings.data.min_absent === v ? true : false}>
 											{v}
 										</option>
 									);
@@ -370,28 +333,22 @@ export default function Settings() {
 							<span>번 부터</span>
 						</div>
 					</div>
-					<div className="input">
-						<div className="select_tit">
-							<span className="bold">패널티 기간</span>
+					<div className='input'>
+						<div className='select_tit'>
+							<span className='bold'>패널티 기간</span>
 						</div>
-						<div className="select_input">
+						<div className='select_input'>
 							<select
-								name="catgo1"
-								className="dropdown"
+								name='catgo1'
+								className='dropdown'
 								onChange={(e) => {
 									handleChange(`once_limit_period`, e.target.value);
-								}}
-							>
+								}}>
 								{array.map((v) => {
 									return (
 										<option
 											id={`day-${v}`}
-											selected={
-												settings && settings.data.once_limit_period === v
-													? true
-													: false
-											}
-										>
+											selected={settings && settings.data.once_limit_period === v ? true : false}>
 											{v}
 										</option>
 									);
@@ -400,36 +357,31 @@ export default function Settings() {
 							<span>일 동안</span>
 						</div>
 					</div>
-					<div className="setting_ex">
+					<div className='setting_ex'>
 						<p>[설정 예시]</p>
-						<span>결석 {postSettings.min_absent}회</span> 부터 결석 시,{" "}
+						<span>결석 {postSettings.min_absent}회</span> 부터 결석 시,{' '}
 						<span>{postSettings.once_limit_period}일간</span> 이용제한 <br />
 						<span>{postSettings.max_absent}회</span> 이상 결석 시, 해당 학기 이용 제한
 					</div>
 
-					<div className="input">
-						<div className="select_tit">
-							<span className="bold">유학생 결과입력</span>
+					<div className='input'>
+						<div className='select_tit'>
+							<span className='bold'>유학생 결과입력</span>
 						</div>
-						<div className="select_input">
+						<div className='select_input'>
 							<select
-								name="catgo1"
-								className="dropdown"
+								name='catgo1'
+								className='dropdown'
 								onChange={(e) => {
 									handleChange(`result_input_deadline`, e.target.value);
-								}}
-							>
+								}}>
 								{array.map((v) => {
 									return (
 										<option
 											id={`day-${v}`}
 											selected={
-												settings &&
-												settings.data.result_input_deadline === v
-													? true
-													: false
-											}
-										>
+												settings && settings.data.result_input_deadline === v ? true : false
+											}>
 											{v}
 										</option>
 									);
@@ -440,41 +392,50 @@ export default function Settings() {
 					</div>
 				</div>
 			</div>
-			<div className="setting_btn_wrap">
-				<div className="gray" onClick={handleOpenForInsertForeignerStudent}>
+			<div className='setting_btn_wrap'>
+				<div className='gray' onClick={handleOpenForInsertForeignerStudent}>
 					유학생 등록
 				</div>
 				<div
-					className="gray"
+					className='gray'
 					onClick={() => {
 						getAdminExportForeigner();
-					}}
-				>
+					}}>
 					유학생 목록 저장
 				</div>
-				<div className="gray" onClick={handleOpenForCreatSectIsOpen}>
+				<div className='gray' onClick={handleOpenForCreatSectIsOpen}>
 					학기 기간 설정
 				</div>
-				<div className="gray" onClick={handleOpenForGetSectIsOpen}>
+				<div className='gray' onClick={handleOpenForGetSectIsOpen}>
 					학기 기간 조회
 				</div>
 
-				<div className="gray" onClick={handleOpenForCreateDept}>
+				<div className='gray' onClick={handleOpenForCreateDept}>
 					학과 등록
 				</div>
 
+				<div className='gray' onClick={handleOpenForReset}>
+					무서운 버튼...
+				</div>
+
 				<div
-					className="save"
+					className='save'
 					onClick={() => {
 						postAdminSetting(postSettings).then(() => {
 							window.location.reload();
 						});
-					}}
-				>
+					}}>
 					저장
 				</div>
 			</div>
 
+			<Modal isOpen={isOpenForReset} handleClose={handleCloseForReset}>
+				<DeleteModal
+					onSubmit={handleReset}
+					message={'테스트메세지입니당'}
+					handleClose={handleCloseForReset}
+				/>
+			</Modal>
 			<Modal isOpen={isOpenForCreateDept} handleClose={handleCloseForCreateDept}>
 				<CreateDept handleClose={handleCloseForCreateDept} />
 			</Modal>
@@ -486,13 +447,12 @@ export default function Settings() {
 			</Modal>
 			<Modal
 				isOpen={isOpenForInsertForeignerStudent}
-				handleClose={handleCloseForInsertForeignerStudent}
-			>
+				handleClose={handleCloseForInsertForeignerStudent}>
 				<InsertForeignerStudent handleClose={handleCloseForInsertForeignerStudent} />
 			</Modal>
 		</div>
 	) : (
-		<div className="content">
+		<div className='content'>
 			<Loader />
 		</div>
 	);
