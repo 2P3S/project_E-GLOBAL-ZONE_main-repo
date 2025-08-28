b9d66d5c148e1fad00a41ea345f6d84f62796994
Soyeon Lim <156628006+dlatth@users.noreply.github.com>
2025-08-29 03:39:16 +0900

    [EDIT] 스케줄 조회 컨테이너 수정 및 온라인/오프라인 필터링 구현

--- E_Global_Zone-Web_build/src/api/korean/index.js
+++ E_Global_Zone-Web_build/src/api/korean/index.js
@@ -1,21 +1,53 @@
-import { korean, koreanLogin } from "../axios";
+import { korean, koreanLogin } from '../axios';
 
 /**
  * get a list of sections has attended
  * return axios get promise instance
  */
-export const getKoreanSection = () => korean.get("section");
+export const getKoreanSection = () => korean.get('section');
 
 /**
  * create an account for korean students
  * @param {object} data {std_kor_id, std_kor_dept, std_kor_name, std_kor_phone, std_kor_mail}
  */
-export const postKoreanAccount = (data) => korean.post("account", data);
+export const postKoreanAccount = (data) => korean.post('account', data);
 
 /**
  * get a schedule list that is can applicate today
+ * @param {string} sch_id - optional schedule ID
+ * @param {boolean} isOffline - optional filter for offline schedules
  */
-export const getKoreanSchedule = (sch_id) => korean.get("schedule" + (sch_id ? `/${sch_id}` : ""));
+export const getKoreanSchedule = (sch_id, isOffline = null) => {
+  let url = 'schedule';
+  if (sch_id) {
+    url += `/${sch_id}`;
+  }
+
+  const params = {};
+  if (isOffline !== null) {
+    params.is_offline = isOffline;
+  }
+
+  return korean.get(url, { params });
+};
+
+/**
+ * get all schedules for a specific date (admin API - no date restrictions)
+ * @param {string} searchDate - date to search for (YYYY-MM-DD)
+ * @param {boolean} isOffline - optional filter for offline schedules
+ */
+export const getAllSchedulesByDate = (searchDate, isOffline = null) => {
+  const params = {
+    search_date: searchDate,
+    guard: 'admin',
+  };
+
+  if (isOffline !== null) {
+    params.is_offline = isOffline;
+  }
+
+  return korean.get('schedule', { params });
+};
 
 export const getKoreanSectionRank = (sect_id) => korean.get(`section/rank/${sect_id}`);
 export const getKoreanSetting = (sect_id) => korean.get(`setting`);
